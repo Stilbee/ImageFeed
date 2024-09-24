8b48a23d105f276573b2a818c584bf73aca130a2
alibi <mailan.alibi.97@yandex.kz>
2024-09-24 16:17:23 +0500

    sprint 9 vvedenie

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Alibi on 23.09.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
+    @IBOutlet var helloWorld: UILabel!
+    @IBOutlet var nickName: UILabel!
+    @IBOutlet var nameSurname: UILabel!
+    @IBOutlet var profilePhoto: UIImageView!
+    override func viewDidLoad() {
+        
+    }
+}

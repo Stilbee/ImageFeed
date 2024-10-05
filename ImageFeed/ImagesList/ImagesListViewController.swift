6f3bfdd6183a917f26f39fc77b0a80b3f05840d7
alibi <mailan.alibi.97@yandex.kz>
2024-10-05 18:29:24 +0500

    complete sprint 10

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -22,7 +22,7 @@ class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
